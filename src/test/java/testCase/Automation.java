85c675c2ebefca467ce8be3dc5d85efb5272a5ed
babue <babufsp2@gmail.com>
2025-05-09 23:33:50 +0530

    new method were added

--- src/test/java/testCase/Automation.java
+++ src/test/java/testCase/Automation.java
@@ -5,5 +5,8 @@ public class Automation {
 	public static void main(String[] args) {
 		System.out.println("Print statement");
 	}
+	public void checkMyMethodCodeChange() {
+		System.out.println("my method");
+	}
 
 }
